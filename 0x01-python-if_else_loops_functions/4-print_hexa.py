ecb084aa15fc1cd9f4a4c41cc77f93e388d35877
daniellator02 <daniellator02@gmail.com>
2023-04-04 13:53:50 -0700

    functions and loops

---
+++ 0x01-python-if_else_loops_functions/4-print_hexa.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+
+for number in range(99):
+    print("{:d} = 0x{:x}".format(number, number))
+Give feedback
+
---
+++ 0x01-python-if_else_loops_functions/add.py
@@ -0,0 +1,4 @@
+#!/usr/bin/python3
+import add_module
+
+add_module.add(5,8)
